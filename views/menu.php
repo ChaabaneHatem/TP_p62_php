89381620835d82b98353e8954f763a8e59b695e3
ChaabaneHatem <hatemchaabane0@gmail.com>
2017-08-17 20:45:51 -0400

    styler le menu intermediaire

--- views/menu.php
+++ views/menu.php
@@ -1,6 +1,7 @@
-<nav>
+<nav id="menu">
     <ul>
         <li><a href="index.php">accueil</a></li>
+        <li><a href="catalague.php">catalague</a></li>
         <li><a href="contact.php">contact</a></li>
     </ul>
 </nav>
--- views/page_top.php
+++ views/page_top.php
@@ -4,15 +4,28 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>$Title$</title>
+  <title>accueil</title>
     <link rel="stylesheet" href="style/main.css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 </head>
 <body>
 <div id="wrapper">
     <header>
-        <?php
+        <div id="info_supplimentaire">
+
+        </div>
+        <div id="logo_panier">
+            <img src="images/logo-bijouterie-montreal.png" alt="logo_bijouterie_MTL">
+            <div id="pannier">
+                <span>0</span>
+                <a href="panier.php"><img src="" alt=""></a>
+            </div>
+        </div>
+        <div id="menu">
+            <?php
             require_once "menu.php"
-        ?>
+            ?>
+        </div>
+
     </header>
 
