66d2d43dcd21565610c6f0d8bedad01f56fb299f
telasmar <tania.asmar5@gmail.com>
2017-08-19 15:38:22 -0400

    modification footer

--- views/page_bottom.php
+++ views/page_bottom.php
@@ -1,7 +1,8 @@
 <?php
 ?>
 <footer>
-    <div>
+    <div id="footer-wrapper">
+    <div class="footer-menu">
         <ul>
             <li><h2>lien du site</h2></li>
             <li><a href="index.php">accueil</a></li>
@@ -9,25 +10,25 @@
             <li><a href="contact.php">contact</a></li>
         </ul>
     </div>
-    <div>
+    <div class="footer-menu">
     <ul>
         <li><h2>Magasin</h2></li>
         <li>255 Boul. Cremazie E.</li>
         <li>Montreal, QC, H2M 1M2</li>
-        <li>Metro Cremazie</li>
         <li>(514) 234-4567</li>
         <li>Heures d’ouverture:</li>
-        <li>lundi-vendredi: 10h-20h</li>
+        <li>lundi-Dimanche: 10h-20h</li>
         <li>samedi-dimanche: 10h-17h</li>
     </ul>
     </div>
-    <div>
+    <div class="footer-menu">
         <ul>
             <li><h2>Nous suivre</h2></li>
             <li><a href="#">Facebook</a></li>
             <li><a href="#">Instagram</a></li>
             <li><a href="#">Pinterest</a></li>
         </ul>
+    </div>
     </div>
 </footer>
 </div>
