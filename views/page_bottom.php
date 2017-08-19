44dcf3840a114fe88ae7aacd7b1ee8f5ea3b59ac
telasmar <tania.asmar5@gmail.com>
2017-08-19 14:14:45 -0400

    Ajout du html footer

--- views/page_bottom.php
+++ views/page_bottom.php
@@ -1,6 +1,34 @@
 <?php
 ?>
 <footer>
+    <div>
+        <ul>
+            <li><h2>lien du site</h2></li>
+            <li><a href="index.php">accueil</a></li>
+            <li><a href="catalogue.php">Bijoux</a></li>
+            <li><a href="contact.php">contact</a></li>
+        </ul>
+    </div>
+    <div>
+    <ul>
+        <li><h2>Magasin</h2></li>
+        <li>255 Boul. Cremazie E.</li>
+        <li>Montreal, QC, H2M 1M2</li>
+        <li>Metro Cremazie</li>
+        <li>(514) 234-4567</li>
+        <li>Heures d’ouverture:</li>
+        <li>lundi-vendredi: 10h-20h</li>
+        <li>samedi-dimanche: 10h-17h</li>
+    </ul>
+    </div>
+    <div>
+        <ul>
+            <li><h2>Nous suivre</h2></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Pinterest</a></li>
+        </ul>
+    </div>
 </footer>
 </div>
 </body>
