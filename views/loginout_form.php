8d7238280e632b9075389cc4bd39d3db3294ca91
telasmar <tania.asmar5@gmail.com>
2017-08-20 17:33:45 -0400

    modification connection

--- views/loginout_form.php
+++ views/loginout_form.php
@@ -3,16 +3,26 @@ require_once "page_top.php"
 ?>
 <div id="loginout">
 <?php if (user_is_logged()) { ?>
-    <form method="post" id="logoutform">
+    <h2>Deconnection de votre compte</h2>
+    <form method="post" id="form-logout">
         <input type="submit" name="logout" value="Déconnexion"/>
     </form>
 <?php } else { ?>
-    <form method="post" id="loginform">
-        <label for="username">Identifiant :</label>
-        <input type="text" name="username" id="username" />
-        <label for="password">Mot de passe: </label>
-        <input type="password" name="password" id="password" />
-        <input type="submit" name="login" value="Connexion"/>
+    <h2>Veuillez vous connecter dans votre compte</h2>
+    <form method="post" id="form-login">
+        <ul>
+            <li>
+                <label for="username">Identifiant :</label>
+                <input type="text" name="username" id="username" />
+            </li>
+            <li>
+                <label for="password">Mot de passe: </label>
+                <input type="password" name="password" id="password" />
+            </li>
+            <li>
+                <input type="submit" name="login" value="Connexion"/>
+            </li>
+        </ul>
     </form>
 <?php } ?>
 </div>
