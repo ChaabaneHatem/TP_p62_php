49fc2d46dcaed4c44ed27dd90e41e546c28111e1
telasmar <tania.asmar5@gmail.com>
2017-08-21 13:14:50 -0400

    resoudre conflit de merge

--- confirmation-formulaire.php
+++ confirmation-formulaire.php
@@ -4,8 +4,9 @@ require_once "views/page_top.php";
 <main>
     <img id="banner-confirm" src="images/banner-confirm.jpeg" alt="">
     <div id="confirmation">
+        <h2>C'est confirmé !</h2>
         <p>Nous avons bien reçu votre message,nous vous répondrons sous peu!</p>
-        <a href="index.php">Retour a l'accueil</a>
+        <a href="index.php">Retour à l'accueil</a>
     </div>
 </main>
 <?php
