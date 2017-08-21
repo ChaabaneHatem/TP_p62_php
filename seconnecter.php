77944f2617c2c93c2502b279ef08a2681922e265
ChaabaneHatem <hatemchaabane0@gmail.com>
2017-08-21 18:42:57 -0400

    la connexion et la deconnection de la page

--- seconnecter.php
+++ seconnecter.php
@@ -1,13 +1,28 @@
 <?php
-require_once 'function/loginout.php';
+
+require_once 'views/page_top.php';
+
 ?>
 
-    <?php require_once 'views/loginout_form.php'; ?>
-    <nav>
-        <ul>
-            <li><a href="private_page.php">Page privée</a></li>
-        </ul>
-    </nav>
+<h2>Veuillez vous connecter dans votre compte</h2>
+<form action="function/loginout.php" method="post" id="form-login">
+    <ul>
+        <li>
+            <label for="username">Identifiant :</label>
+            <input type="text" name="username" id="username" />
+        </li>
+        <li>
+            <label for="password">Mot de passe: </label>
+            <input type="password" name="password" id="password" />
+        </li>
+        <li>
+            <input type="submit" name="login" value="Connexion"/>
+        </li>
+    </ul>
+</form>
+
 <?php
+
 require_once 'views/page_bottom.php';
+
 ?>
